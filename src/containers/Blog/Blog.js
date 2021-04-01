7bde229879d2d6f0d133958a5737999806cfdd56
Julian Capilla <lyhan_jr@hotmail.com>
2021-04-01 15:18:50 +0200

    Using API

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,39 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response=>{
+                let posts = response.data;
+                let updatedPosts = posts.slice(0,4).map(post =>{
+                    return {
+                        ...post,
+                        author: "Max"
+                    }
+                })
+                this.setState({posts:updatedPosts});
+            })
+    }
+    postSelectedHandler = (id)=>{
+        this.setState({selectedPostId: id})
+    }
     render () {
+        let {posts} = this.state;
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {this.state.posts.map(post=>(
+                        <Post title={post.title}
+                              author={post.author}
+                              clicked={()=>this.postSelectedHandler(post.id)}
+                        />))}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
