c54a78ce952ab1bdffb76ce304aae180403cdd4c
Julian Capilla <lyhan_jr@hotmail.com>
2021-04-02 20:11:16 +0200

    More navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-import {Route,Link} from 'react-router-dom';
+import {Route,NavLink} from 'react-router-dom';
 
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import  './Blog.css';
-
+/*
+NavLink adds "active" css class
+To pass routing props use hoc 'withRouter' to wrap components
+ */
 class Blog extends Component {
     render () {
         return (
             <div>
                 <header className="Blog">
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to={{
-                            pathname:'/new-post',
+                        <li><NavLink
+                            to="/"
+                            exact
+                            activeClassName="my-custom-class"
+                            activeStyle={{
+                                textDecoration:'underline',
+                                color: '#fa923f'
+                            }}
+
+                        >Home</NavLink></li>
+                        <li><NavLink to={{
+                            pathname: '/new-post',
                             hash: '#myhash',
-                            search:'?query=params'
-                        }}>New Post</Link></li>
+                            search: '?query=params'
+                        }}>New Post</NavLink></li>
                     </ul>
                 </header>
                 <Route path="/" exact component={Posts}/>
