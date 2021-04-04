5e0b664164f0c8948a62e867879903454b2d9578
Julian C <jca@bigtincan.com>
2021-04-04 18:18:56 +0200

    Domain caveat

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
     render() {
         return (
+            // If domain serving the app is not root e.g. example.com/my-app
+            // <BrowserRouter basename="my-app">
             <BrowserRouter>
                 <div className="App">
                     <Blog/>
