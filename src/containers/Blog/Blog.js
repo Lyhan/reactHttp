82259b22d0ea61dfb24af3e85643a1014fd79366
Julian C <jca@bigtincan.com>
2021-04-04 18:06:15 +0200

    Lazy loading using React.lazy

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import {Route,NavLink,Switch} from 'react-router-dom';
+import React, {Component,Suspense} from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
-import Posts from "./Posts/Posts";
 // import NewPost from "./NewPost/NewPost";
-import  './Blog.css';
+
+import './Blog.css';
 import FullPost from "./FullPost/FullPost";
-import {Redirect} from "react-router";
+import {Redirect} from "react-router-dom";
 import asyncComponent from '../../hoc/asyncComponent';
+import Posts from "./Posts/Posts";
+const LazyLoadedNewPost = React.lazy(() => import('./NewPost/NewPost'));
 
 /*
 NavLink adds "active" css class
 To pass routing props use hoc 'withRouter' to wrap components
  */
 
-const AsyncNewPost = asyncComponent(()=>{
+const AsyncNewPost = asyncComponent(() => {
     return import('./NewPost/NewPost');
 });
+
+
 class Blog extends Component {
     state = {
         auth: true
     }
-    render () {
+
+    render() {
         return (
-            <div>
+            <React.Fragment>
                 <header className="Blog">
                     <ul>
                         <li><NavLink
@@ -30,7 +35,7 @@ class Blog extends Component {
                             exact
                             activeClassName="my-custom-class"
                             activeStyle={{
-                                textDecoration:'underline',
+                                textDecoration: 'underline',
                                 color: '#fa923f'
                             }}
 
@@ -45,16 +50,17 @@ class Blog extends Component {
                 <Switch>
                     {/*Guard against Unauthenticated users, if component isn't rendered,
                      there is no way to reach it */}
-                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null }
+                    {/*{this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}*/}
+                    <Route path="/new-post" render={()=><Suspense fallback={<div>Loading...</div>}><LazyLoadedNewPost/></Suspense>}/>
                     <Route path="/posts" component={Posts}/>
                     {/*Multiple Routes pointing to same Component is ok*/}
                     <Route path="/" exact component={Posts}/>
                     {/*<Redirect from="/" to="/posts"/>*/}
                     {/*Catch all routes not found, it does not work with Redirect from='/'*/}
-                    <Route render={()=><h1 style={{textAlign:'center'}}>Not found</h1>}/>
+                    <Route render={() => <h1 style={{textAlign: 'center'}}>Not found</h1>}/>
 
                 </Switch>
-            </div>
+            </React.Fragment>
         );
     }
 }
